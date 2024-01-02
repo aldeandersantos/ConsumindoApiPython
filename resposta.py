d5610b1da7827537e6ddf7f0e2b3b1187fdd93d4
Aldeander <aldeander-santos@hotmail.com>
2024-01-02 17:34:04 -0300

    Adicionado função de escrever no banco de dados

--- resposta.py
+++ resposta.py
@@ -1,26 +1,48 @@
 import requests
-import json
+import pyodbc
 
 def requisicao_api():
     try:
-        cep = input("Digite o CEP do local que você quer: ")
-        api_url = "https://brasilapi.com.br/api/cep/v1/"+cep
+        entrada = input("Digite o CEP do local que você quer: ")
+        api_url = "https://brasilapi.com.br/api/cep/v1/"+entrada
 
         resposta = requests.get(api_url)
 
+        
         if resposta.status_code == 200:
             dados = resposta.json()
-
-            print("-----------------------")
-            print("Serviço:", dados['service'])
-            print("Cep:", dados['cep'][:5] + "-" + dados['cep'][5:])
-            print("Logradouro:", dados['street'])
-            print("Bairro:", dados['neighborhood'])
-            print("Cidade:", dados['city']+", "+dados['state'])
-
+            conexao_bdd(dados) 
         else:
             print("Erro ao fazer requisição. Código:", resposta.status_code)
+        
 
     except requests.exceptions.RequestException as e:
         print("Erro de requisição:", e)
+
+def conexao_bdd(dados):
+    dados_conexao = (
+        "Driver={SQL Server};"
+        "Server=ALDEANDER-PC;"
+        "Database=PythonSQL;"
+    )
+    conexao = pyodbc.connect(dados_conexao)
+    print("Conexão bem sucedida!")
+    cursor = conexao.cursor()
+
+    servico = dados['service']
+    cep = dados['cep']
+    logradouro = dados['street']
+    bairro = dados['neighborhood']
+    cidade = dados['city']
+    estado = dados['state']
+
+
+    comando = f"""INSERT INTO Enderecos(Servico, CEP, Logradouro, Bairro, Cidade, Estado)
+            VALUES
+	        ('{servico}', '{cep}', '{logradouro}', '{bairro}', '{cidade}', '{estado}')"""
+    
+    cursor.execute(comando)
+    cursor.commit()
+
+
 requisicao_api()
