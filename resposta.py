625e1653710c99685fadb7022db95521a096948d
Aldeander <aldeander-santos@hotmail.com>
2024-01-03 12:55:33 -0300

    Adicionado função de vários endereços

--- resposta.py
+++ resposta.py
@@ -3,18 +3,24 @@ import pyodbc
 
 def requisicao_api():
     try:
-        entrada = input("Digite o CEP do local que você quer: ")
-        api_url = "https://brasilapi.com.br/api/cep/v1/"+entrada
+        while True:
+            entrada = input("Digite o CEP do local que você quer: ")
+            print ("Analisando o cep informado...")
+            api_url = "https://brasilapi.com.br/api/cep/v1/"+entrada
 
-        resposta = requests.get(api_url)
+            resposta = requests.get(api_url)
 
-        
-        if resposta.status_code == 200:
-            dados = resposta.json()
-            conexao_bdd(dados) 
-        else:
-            print("Erro ao fazer requisição. Código:", resposta.status_code)
-        
+            
+            if resposta.status_code == 200:
+                dados = resposta.json()
+                conexao_bdd(dados) 
+            else:
+                print("Erro ao fazer requisição. Código:", resposta.status_code)
+            
+            continuar = input("------------------------------------------\nDeseja adicionar outro CEP? (s/n) ")
+
+            if continuar.lower() != 's':
+                break
 
     except requests.exceptions.RequestException as e:
         print("Erro de requisição:", e)
@@ -26,15 +32,22 @@ def conexao_bdd(dados):
         "Database=PythonSQL;"
     )
     conexao = pyodbc.connect(dados_conexao)
-    print("Conexão bem sucedida!")
     cursor = conexao.cursor()
 
     servico = dados['service']
     cep = dados['cep']
-    logradouro = dados['street']
-    bairro = dados['neighborhood']
     cidade = dados['city']
     estado = dados['state']
+
+    if 'street' in dados:
+        logradouro = dados['street']
+    else:
+        logradouro = "Rua desconhecida"
+
+    if 'neighborhood' in dados:
+        bairro = dados['neighborhood']
+    else:
+        bairro = "Bairro desconhecido"
 
 
     comando = f"""INSERT INTO Enderecos(Servico, CEP, Logradouro, Bairro, Cidade, Estado)
@@ -45,4 +58,40 @@ def conexao_bdd(dados):
     cursor.commit()
 
 
+def imprimir_dados():
+    dados_conexao = (
+        "Driver={SQL Server};"
+        "Server=ALDEANDER-PC;"
+        "Database=PythonSQL;"
+    )
+
+    conexao = pyodbc.connect(dados_conexao)
+    cursor = conexao.cursor()
+    cursor.execute("SELECT Servico, CEP, Logradouro, Bairro, Cidade, Estado FROM Enderecos")
+
+    print("Seus endereços solicitados foram:")
+    for linha in cursor.fetchall():
+        print("Serviço:", linha[0])
+        print("CEP:", linha[1])
+        print("Logradouro:", linha[2])
+        print("Bairro:", linha[3])
+        print("Cidade:", linha[4], ",", linha[5])
+        print ("------------------------------------------")
+
+def limpar_banco():
+    dados_conexao = (
+        "Driver={SQL Server};"
+        "Server=ALDEANDER-PC;"
+        "Database=PythonSQL;"
+    )
+    conexao = pyodbc.connect(dados_conexao)
+    cursor = conexao.cursor()
+
+    comando = "DELETE FROM Enderecos"
+    cursor.execute(comando)
+    conexao.commit()
+
 requisicao_api()
+imprimir_dados()
+
+limpar_banco()
